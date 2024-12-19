caca69c18a0d96cff4145b5d5da70e5a60c4a1bc
shiron <shiron4710.dev@gmail.com>
2024-12-19 11:36:31 +0900

    test: change cmd test to table driven test

--- cmd/gen/iterm_test.go
+++ cmd/gen/iterm_test.go
@@ -8,5 +8,38 @@ func TestItermCmd(t *testing.T) {
 	t.Parallel()
 
 	configFilePath, tempDir := genTestDir(t)
-	itermCmd.Run(itermCmd, []string{configFilePath, tempDir})
+
+	type args struct {
+		args []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "2 args",
+			args: args{args: []string{configFilePath, tempDir}},
+		},
+		{
+			name: "1 arg",
+			args: args{args: []string{configFilePath}},
+		},
+		{
+			name: "0 args",
+			args: args{args: []string{}},
+		},
+		{
+			name: "reversed args",
+			args: args{args: []string{tempDir, configFilePath}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			itermCmd.Run(itermCmd, tt.args.args)
+		})
+	}
 }
--- cmd/gen/vscode_test.go
+++ cmd/gen/vscode_test.go
@@ -8,5 +8,38 @@ func TestVSCodeCmd(t *testing.T) {
 	t.Parallel()
 
 	configFilePath, tempDir := genTestDir(t)
-	vscodeCmd.Run(vscodeCmd, []string{configFilePath, tempDir})
+
+	type args struct {
+		args []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "2 args",
+			args: args{args: []string{configFilePath, tempDir}},
+		},
+		{
+			name: "1 arg",
+			args: args{args: []string{configFilePath}},
+		},
+		{
+			name: "0 args",
+			args: args{args: []string{}},
+		},
+		{
+			name: "reversed args",
+			args: args{args: []string{tempDir, configFilePath}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			vscodeCmd.Run(vscodeCmd, tt.args.args)
+		})
+	}
 }
