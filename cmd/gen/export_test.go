2eba4aa32ab2e40f44839cf950b747f2e0a018f3
shiron <shiron4710.dev@gmail.com>
2024-12-19 11:17:28 +0900

    test: e2e test to blackbox test

--- cmd/gen/export_test.go
+++
@@ -1,3 +0,0 @@
-package gen
-
-var VSCodeCmd = vscodeCmd
--- cmd/gen/vscode_test.go
+++ cmd/gen/vscode_test.go
@@ -1,31 +1,14 @@
-package gen_test
+package gen
 
 import (
 	"io"
 	"os"
 	"path"
 	"testing"
-
-	"github.com/shiron-dev/arcanum-hue/cmd/gen"
 )
 
-var originalArgs = os.Args
-
-func setArgs(arg string) {
-	//nolint:gocritic
-	os.Args = append(originalArgs, arg)
-}
-
-func resetArgs() {
-	os.Args = originalArgs
-}
-
-func Test_vscodeCmd(t *testing.T) {
+func TestVSCodeCmd(t *testing.T) {
 	t.Parallel()
-
-	setArgs("sub")
-
-	defer resetArgs()
 
 	tempDir := t.TempDir()
 
@@ -58,5 +41,5 @@ func Test_vscodeCmd(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	gen.VSCodeCmd.Run(gen.VSCodeCmd, []string{configFilePath, tempDir})
+	vscodeCmd.Run(vscodeCmd, []string{configFilePath, tempDir})
 }
